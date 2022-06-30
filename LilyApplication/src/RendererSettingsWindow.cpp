2e299c5dcb9bf60ee875f9486e822a0ceb9cb73b
Erik Ramsey <erikr.ramsey@gmail.com>
2022-06-30 15:46:29 -0700

    Dynamic shader compilation and materials

--- LilyApplication/src/RendererSettingsWindow.cpp
+++ LilyApplication/src/RendererSettingsWindow.cpp
@@ -17,20 +17,27 @@ void RendererSettingsWindow::render() {
     ImGui::End();
 }
 void RendererSettingsWindow::shader_list() {
-    static int selection = -1;
     if (!fs::exists(m_shader_directory)) {
         ImGui::Text("No shader directory found");
         return;
     }
-    auto it = 0;
-    for (auto entry : fs::directory_iterator(m_shader_directory)) {
-        if (ImGui::Selectable(entry.path().string().c_str(), it == selection)) {
-            selection = it;
-        }
 
-        if (ImGui::IsMouseDoubleClicked(0)) {
-            Renderer::build_shader(entry.path());
+    static fs::path vs_path;
+    for (auto entry : fs::directory_iterator(m_shader_directory)) {
+        if (ImGui::Selectable(entry.path().string().c_str(), entry.path() == vs_path)) {
+            vs_path = entry.path();
         }
-        it++;
+    }
+
+    ImGui::Dummy(ImVec2(0, 10));
+    static fs::path fs_path;
+    for (auto entry : fs::directory_iterator(m_shader_directory)) {
+        if (ImGui::Selectable((entry.path().string() + "##").c_str(), entry.path() == fs_path)) {
+            fs_path = entry.path();
+        }
+    }
+
+    if (ImGui::Button("Build shaders")) {
+        Renderer::build_and_use_shader(vs_path, fs_path);
     }
 }
